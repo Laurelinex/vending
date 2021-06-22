16a4f2969a476b7dbe8f36cfa51d39bdc4aa48b7
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-06-22 12:02:47 +0100

    Update CMakeDependencies.cmake

--- CMakeDependencies.cmake
+++ CMakeDependencies.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 
 FetchContent_Declare(catch2
-        GIT_REPOSITORY git@github.com:catchorg/Catch2.git
+        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
         GIT_TAG 5c88067bd339465513af4aec606bd2292f1b594a)
 FetchContent_GetProperties(catch2)
 
@@ -10,7 +10,7 @@ if(NOT catch2_POPULATED)
 endif()
 
 FetchContent_Declare(trompeloeil
-        GIT_REPOSITORY git@github.com:rollbear/trompeloeil.git
+        GIT_REPOSITORY https://github.com/rollbear/trompeloeil.git
         GIT_TAG 83f7d0cdbb08c68fe29b17c6672ae845a301a103)
 FetchContent_GetProperties(trompeloeil)
 
